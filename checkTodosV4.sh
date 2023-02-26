979e42a6f1edba4d74b9e22a9a2867cda765393d
ehsan <ehsan.mujahid1798@gmail.com>
2023-02-26 12:41:03 +0400

    The upload commit

---
+++ checkTodosV4.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+
+
+# The new checkTodos !
+#	No replacing files !
+#	No multiple greps !
+#	2 Creation ops
+#	1 Move op
+#	1 Grep op
+
+# Todos
+#	format: todos_${DATE}
+#	experiments
+#			acquire that "empty" space character
+# Backups
+#	format: no change
+#	just move the file to this directory
+#
+# Completed
+#	format: todos_${DATE}
+#	generate file from siphoned completed todos
+#	
+#	
+#
+#	
+#	
+#	
+#	
+#	
+#	
+#	
+#	
+#
+#	
+#	
+#	
+#
