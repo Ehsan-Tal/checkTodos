59c59ecc1f795cf1314f8485a6964eafba7cfe47
ehsan <ehsan.mujahid1798@gmail.com>
2023-03-06 21:46:52 +0400

    message tab limits

--- checkTodosV4.sh
+++ checkTodosV4.sh
@@ -7,6 +7,13 @@ NC="\e[0m"
 
 PASS="✔️ "
 FAIL="❌"
+TABCHARACTERLIMIT="48"
+
+#TODO: figure out the character limit for tabs
+#TODO: figure out the tab:character ratio
+#TODO: figure out a control structure that fits the bill
+#		this control structure can allow us to refactor into an error and an exit
+
 
 function ask_consent() {
         # question is internal, response is external
@@ -19,6 +26,63 @@ function ask_consent() {
 
 }
 
+function exit_script {
+	echo -e "\nMirupafshim, ${USER}"
+	exit 0
+}
+
+function exit_script_with_message {
+	echo " - ${1:-Missing message}"
+	exit_script
+}
+
+function process_error {
+	message=" -- ERROR: ${1}"
+
+	if [ "${#message}" -le ${TABCHARACTERLIMIT} ]; then
+		echo "${message}"
+	else
+		echo " -- ERROR: ${#message} is too long, lol."
+	fi
+
+	unset message
+}
+
+function subprocess_error {
+	message=" --- ERROR: ${1}"
+
+	if [ "${#message}" -le ${TABCHARACTERLIMIT} ]; then
+		echo "${message}"
+	else
+		echo " --- ERROR: ${#message} is too long, lol."
+	fi
+
+	unset message
+}
+
+function process_success {
+	message=" -- success: ${1}"
+
+	if [ "${#message}" -le ${TABCHARACTERLIMIT} ]; then
+		echo "${message}"
+	else
+		echo " -- success: ${#message} is too long, lol."
+	fi
+
+	unset message
+}
+
+function subprocess_success {
+	message=" --- success: ${1}"
+
+	if [ "${#message}" -le ${TABCHARACTERLIMIT} ]; then
+		echo "${message}"
+	else
+		echo " --- success: ${#message} is too long, lol."
+	fi
+
+	unset message
+}
 
 
 # The new checkTodos !
@@ -76,10 +140,10 @@ for (( i=0; i < "${#DIRECTORYFILEPATHArray[*]}"; i++ )); do
 	DIRECTORYFILEPATH="${DIRECTORYFILEPATHArray[${i}]}"
 
 	if [ -d "${DIRECTORYFILEPATH}" ]; then
-		echo " --- success: $(basename ${DIRECTORYFILEPATH}) exists					${PASS}"
+		subprocess_success "$(basename ${DIRECTORYFILEPATH}) exists					${PASS}"
 	
 	else
-		echo " --- ERROR: $(basename ${DIRECTORYFILEPATH}) missing					${FAIL}"
+		subprocess_success "$(basename ${DIRECTORYFILEPATH}) missing					${FAIL}"
         	ask_consent "Do you want to create ${DIRECTORYFILEPATH} ?"
 	
 		if [ ${consent} = "Y" ]; then
@@ -88,8 +152,8 @@ for (( i=0; i < "${#DIRECTORYFILEPATHArray[*]}"; i++ )); do
 			#TODO: unhash when done
 			#mkdir ${DIRECTORYFILEPATH}
 		else
-			echo -e "\n - Exiting"
-        		exit 0
+			echo -e "CONSENT NEGATIVE"
+        		exit_script
 		fi
 
 		unset consent
@@ -102,37 +166,59 @@ done
 unset DIRECTORYFILEPATHArray
 
 
-echo -e "\n${YELLOW} - NOTE:${NC} Todo is not implemented"
 echo -e "\n - Gathering Todo (T) input"
+echo -e "${YELLOW} - NOTE:${NC} Todo is not implemented"
 
-echo " -- find out T file..."
-
-[ -s "${NEWTODOFILEPATH}" ] || touch ${NEWTODOFILEPATH}
-
-OLDTODOFILE="$(ls ${ORIGINALDIRECTORYFILEPATH} | grep -i "todos"* )"
-
-OLDTODOFILEPATH="${ORIGINALDIRECTORYFILEPATH}/${OLDTODOFILE}"
-
-echo " --- checking if T file exists..."
-
-echo " -- reading T file contents..."
-mapfile -t CONTENTSArray < <(cat ${OLDTODOFILEPATH})
-
-cat ${CONTENTSArray} | tail -5
-
-echo " --- checking if T data exists..."
-
-echo " ---- generating T file..."
-
-#touch ${}
-#echo "default" > ${NEWTODOFILE}
-
-echo " Done ?"
-read
+while [ 1 ]; do
 
 
-echo -e "\n${YELLOW} - NOTE:${NC} Todos is not implemented"
+#	[ -s "${NEWTODOFILEPATH}" ] || touch ${NEWTODOFILEPATH}
+
+	#OLDTODOFILE="$(ls ${ORIGINALDIRECTORYFILEPATH} | grep -i "todos" )"
+	OLDTODOFILE="$(ls ${ORIGINALDIRECTORYFILEPATH} | grep -i "todos.md")"
+	#TODO fix the grep to be specific to the todo file within
+	OLDTODOFILEPATH="${ORIGINALDIRECTORYFILEPATH}/${OLDTODOFILE}"
+
+	echo " --- checking if T file exists..."
+	if [ -n "${OLDTODOFILE}" ]; then
+		echo " --- reading T file contents..."
+		mapfile -t CONTENTSArray < <(cat ${OLDTODOFILEPATH})
+
+		echo ${CONTENTSArray}
+
+		echo " --- checking if T data exists..."
+		if [ -n "${CONTENTSArray}" ]; then
+			subprocess_success "${OLDTODOFILE} has data					${PASS}"
+			break
+	
+		else 
+			subprocess_error "${OLDTODOFILE:-'missing file'} is empty !					${FAIL}"
+			exit_script
+		fi
+
+	else
+		process_error "${OLDTODOFILE:-The todo file} is absent !				${FAIL}"
+		echo " -- generating T file..."
+		ask_consent "Do you want to generate a new one ?"
+
+		if [ "${consent}" = "Y" ]; then
+			generate_default_todo "${NEWTODOFILE}" || exit_script_with_message "ERROR: generate default todo function not sourced !"
+		else
+			exit_script_with_message "decided not to generate todo file"
+
+		fi
+
+		unset consent
+
+	fi
+
+done
+# END OF T INPUT WHILE LOOP
+
+
+
 echo -e "\n - Processing Todo (T) data"
+echo -e "${YELLOW} - NOTE:${NC} Todos is not implemented"
 
 echo " -- destroying cancelled..."
 
@@ -141,16 +227,16 @@ echo " -- checking if ${DATE} < TODAY..."
 echo " --- extricating completed..."
 
 
-echo -e "\n${YELLOW} - NOTE:${NC} Backups is not implemented"
 echo -e "\n - Fiddling with Backups (B)"
+echo -e "${YELLOW} - NOTE:${NC} Backups is not implemented"
 
 echo " -- checking if ${DATE} < TODAY..."
 
 echo " --- moving B file..."
 
 
-echo -e "\n${RED} - WARNING: ${NC} Domain Todos are not implemented"
 echo -e "\n - Gathering Domain Todos (D) input"
+echo -e "${RED} - WARNING: ${NC} Domain Todos are not implemented"
 
 echo " -- reading D file contents"
 
@@ -161,8 +247,8 @@ echo " -- check against DOMAINS.md..."
 echo " --- generating D file..."
 
 
-echo -e "\n${RED} - WARNING: ${NC} Domain Todos are not implemented"
 echo -e "\n - Processing Domain Todos (D) data"
+echo -e "${RED} - WARNING: ${NC} Domain Todos are not implemented"
 
 echo " -- destroying cancelled..."
 
@@ -175,8 +261,8 @@ echo " -- counting lines..."
 echo " --- update heading with new count..."
 
 
-echo -e "\n${YELLOW} - NOTE:${NC} Completed is not implemented"
 echo -e "\n - Fiddling with Completed (C)"
+echo -e "${YELLOW} - NOTE:${NC} Completed is not implemented"
 
 echo " -- checking if C data exists..."
 
